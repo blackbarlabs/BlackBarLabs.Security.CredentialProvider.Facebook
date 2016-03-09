4e8fbd7a2778c9d5bf19c5ea886fddce0b149fd7
joshcodes <josh@blackbarlabs.com>
2016-03-08 19:19:35 -0600

    initial

---
+++ FacebookCredentialProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Facebook;
+
+namespace BlackBarLabs.Security.CredentialProvider.Facebook
+{
+    public class FacebookCredentialProvider : IProvideCredentials
+    {
+        public async Task<string> RedeemTokenAsync(Uri providerId, string username, string accessToken)
+        {
+            if (String.IsNullOrWhiteSpace(accessToken))
+                return default(string);
+            var client = new FacebookClient(accessToken);
+            dynamic result = await client.GetTaskAsync("me", new { fields = "name,id" });
+            if (null == result)
+                return null;
+            if (username != result.id)
+                return null;
+            return client.AccessToken;
+        }
+    }
+}
