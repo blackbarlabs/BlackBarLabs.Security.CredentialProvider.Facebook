b3e9937f65d43394dd8e72d76e136733c45e06be
joshcodes <josh@blackbarlabs.com>
2016-03-21 01:56:01 -0500

    added more outcome options to RedeemToken

--- FacebookCredentialProvider.cs
+++ FacebookCredentialProvider.cs
@@ -6,17 +6,26 @@ namespace BlackBarLabs.Security.CredentialProvider.Facebook
 {
     public class FacebookCredentialProvider : IProvideCredentials
     {
-        public async Task<string> RedeemTokenAsync(Uri providerId, string username, string accessToken)
+        public async Task<TResult> RedeemTokenAsync<TResult>(Uri providerId, string username, string accessToken,
+            Func<string, TResult> success, Func<TResult> invalidCredentials, Func<TResult> couldNotConnect)
         {
             if (String.IsNullOrWhiteSpace(accessToken))
-                return default(string);
+                return invalidCredentials();
             var client = new FacebookClient(accessToken);
-            dynamic result = await client.GetTaskAsync("me", new { fields = "name,id" });
-            if (null == result)
-                return null;
-            if (username != result.id)
-                return null;
-            return client.AccessToken;
+            try
+            {
+                dynamic result = await client.GetTaskAsync("me", new { fields = "name,id" });
+                if (null == result)
+                    return invalidCredentials();
+                if (username != result.id)
+                    return invalidCredentials();
+                return success(client.AccessToken);
+            } catch(Exception ex)
+            {
+                if (ex.Message.Contains("OAuthException"))
+                    return invalidCredentials();
+                throw ex;
+            }
         }
     }
 }
