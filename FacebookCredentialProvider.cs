ab41d84c5a7fb0fda0ad52747532579b0afe4516
Keith Holloway <keith@eastfive.com>
2016-04-08 11:24:03 -0500

    Added error messaging capabilities for credential checks

--- FacebookCredentialProvider.cs
+++ FacebookCredentialProvider.cs
@@ -7,23 +7,23 @@ namespace BlackBarLabs.Security.CredentialProvider.Facebook
     public class FacebookCredentialProvider : IProvideCredentials
     {
         public async Task<TResult> RedeemTokenAsync<TResult>(Uri providerId, string username, string accessToken,
-            Func<string, TResult> success, Func<TResult> invalidCredentials, Func<TResult> couldNotConnect)
+            Func<string, TResult> success, Func<string, TResult> invalidCredentials, Func<TResult> couldNotConnect)
         {
             if (String.IsNullOrWhiteSpace(accessToken))
-                return invalidCredentials();
+                return invalidCredentials("accessToken is null");
             var client = new FacebookClient(accessToken);
             try
             {
                 dynamic result = await client.GetTaskAsync("me", new { fields = "name,id" });
                 if (null == result)
-                    return invalidCredentials();
+                    return invalidCredentials("Cannot get token from Facebook");
                 if (username != result.id)
-                    return invalidCredentials();
+                    return invalidCredentials("username and result.Id from Facebook do not match");
                 return success(client.AccessToken);
             } catch(Exception ex)
             {
                 if (ex.Message.Contains("OAuthException"))
-                    return invalidCredentials();
+                    return invalidCredentials("OAuthException occurred");
                 throw ex;
             }
         }
